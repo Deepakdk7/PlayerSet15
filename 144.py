b86f84d689a612780ce35572f99d0b8c308d46e3
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-22 12:17:25 +0530

    Update 144.py

--- 144.py
+++ 144.py
@@ -1,9 +1,7 @@
 ax=int(input())
 a=list(map(int,input().split()))
 c=[]
-for i in range(0,ax):
-    for j in range(i+1,ax):
-        if a[j]%a[i]==0:
-            c.append(a[j])
-c=list(dict.fromkeys(c))
+for i in range(0,ax-1):
+        if a[i+1]%a[i]==0:
+            c.append(a[i+1])
 print(*c)
