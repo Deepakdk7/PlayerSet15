b2577cb4b9d03bd971c0c69a1fc93c271fd3a360
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-08-10 17:18:09 +0530

    Update 152.py

--- 152.py
+++ 152.py
@@ -1,12 +1,15 @@
 a=list(input())
 n=0
-v=['a','e','i','o','u']
-c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-for i in range(len(a)-1):
-    if a[i] in v and a[i+1] in c:
+if len(a)>1:
+    v=['a','e','i','o','u']
+    c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+    for i in range(len(a)-1):
+        if a[i] in v and a[i+1] in c:
+            n=n+1
+        if a[i] in c and a[i+1] in v:
+            n=n+1
+    if (a[-1] in c and a[-2] in v) or (a[-1] in v and a[-2] in c):
         n=n+1
-    if a[i] in c and a[i+1] in v:
-        n=n+1
-if (a[-1] in c and a[-2] in v) or (a[-1] in v and a[-2] in c):
-    n=n+1
-print(n)
+    print(n)
+else:
+    print(n)
